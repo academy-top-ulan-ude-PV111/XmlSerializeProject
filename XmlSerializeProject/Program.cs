fba38111dbd033a8c7f41addcf14603f366a63b3
GitStudent2022 <torelgame@mail.ru>
2022-10-19 16:22:30 +0300

    Добавьте файлы проекта.

---
+++ XmlSerializeProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlSerializeProject
+{
+    [Serializable]
+    public class Employe
+    {
+        public string? Name { set; get; }
+        public int Age { set; get; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe employe = new Employe() { Name = "Bob", Age = 34 };
+            
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Employe));
+            
+            using(FileStream file = new FileStream("employes.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(file, employe);
+            }
+
+        }
+    }
+}
